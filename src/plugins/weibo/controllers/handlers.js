2eb59c6b74cd7a23f656c33218fd9caa42002bc5
shixiansi <65282448+shixiansi@users.noreply.github.com>
2026-02-20 15:06:04 +0800

    Delete src/plugins/weibo/controllers/handlers.js

--- src/plugins/weibo/controllers/handlers.js
+++
@@ -1,9 +0,0 @@
-import lodash from "lodash"
-import { segment } from "../../../Bot/segment.js"
-export function register(bot) {
-  if (!bot || !bot.registerCommand) return
-  //第一个参数是数组第一个是命令，第二个是事件,如果是其他事件就是事件列表中的事件名称，第二个是方法，第三个是下文函数
-  bot.registerCommand(["", 2000], async ctx => {
-    return false
-  })
-}
