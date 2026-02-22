a7da0dde56e1d7d8c300ac478793bcdbdfc908da
shixiansi <1765629830@qq.com>
2026-02-22 22:28:13 +0800

    尝试兼容trss

--- src/Bot/icqq/EventListener.js
+++ src/Bot/icqq/EventListener.js
@@ -1,18 +1,18 @@
-import Filemage from "../../utils/Filemage.js"
-import lodash from "lodash"
-import pluginLoader from "./pluginLoader.js"
-import getImageDisplay from "../../utils/imgdisplay.js"
+import Filemage from "../../utils/Filemage.js";
+import lodash from "lodash";
+import pluginLoader from "./pluginLoader.js";
+import getImageDisplay from "../../utils/imgdisplay.js";
 
 const dealMsg = async (e, msg) => {
-  console.log(msg)
-  let imgdisplay
+  console.log(msg);
+  let imgdisplay;
   if (e.user_id === e.self_id) {
-    imgdisplay = await getImageDisplay()
+    imgdisplay = await getImageDisplay();
   }
 
   switch (msg.type) {
     case "text":
-      break
+      break;
     case "image":
       msg = {
         type: "image",
@@ -21,32 +21,34 @@ const dealMsg = async (e, msg) => {
           sub_type: "normal",
           summary: imgdisplay || msg.summary || "",
         },
-      }
-      break
+      };
+      break;
     default:
-      break
+      break;
   }
-  return msg
-}
+  return msg;
+};
 
 const sendMessage = async (ctx, message) => {
   if (ctx.group_id) {
     if (typeof ctx === "string") {
       // 私聊消息 - 将字符串ID转换为数字
       return await Bot.pickFriend(ctx).sendMsg(
-        Array.isArray(message) ? message : [{ type: "text", data: { text: message } }],
-      )
+        Array.isArray(message)
+          ? message
+          : [{ type: "text", data: { text: message } }],
+      );
     } else if (ctx.group_id) {
       // 群聊消息 - 确保group_id是数字
       let msg = Array.isArray(message)
         ? await dealMsg(ctx, message)
         : typeof message === "string"
           ? [{ type: "text", data: { text: message } }]
-          : [await dealMsg(ctx, message)]
-      return await Bot.pickGroup(ctx.group_id).sendMsg(msg)
+          : [await dealMsg(ctx, message)];
+      return await Bot.pickGroup(ctx.group_id).sendMsg(msg);
     }
   }
-}
+};
 
 export default class EventListener {
   /**
@@ -56,10 +58,10 @@ export default class EventListener {
    * @param data.once 是否只监听一次
    */
   constructor(data) {
-    this.prefix = data.prefix || ""
-    this.event = data.event
-    this.once = data.once || false
-    this.plugins = pluginLoader
+    this.prefix = data.prefix || "";
+    this.event = data.event;
+    this.once = data.once || false;
+    this.plugins = pluginLoader;
   }
 }
 
@@ -72,119 +74,131 @@ class ListenerLoader {
    * @param client Bot示例
    */
   async load(client) {
-    console.log(Bot.matchers)
-    this.client = client
-    pluginLoader.Bot = client
-    let filemag = new Filemage(process.cwd() + "/plugins/xunlu-core/src/Bot/icqq/Event")
-    let botenv = this.checkEnv()
+    console.log("原始bot", Bot);
+    this.client = client;
+    pluginLoader.Bot = client;
+    let filemag = new Filemage(
+      process.cwd() + "/plugins/xunlu-core/src/Bot/icqq/Event",
+    );
+    let botenv = this.checkEnv();
 
-    await pluginLoader.initBot()
-    await pluginLoader.runMount()
-    Bot.sendMessage = sendMessage
-    Bot.makeForwardMsg = pluginLoader.makeForwardMsg
-    Bot.renderImg = pluginLoader.renderImg
-    const files = filemag.GetfileList().filter(file => file.endsWith(".js"))
+    await pluginLoader.initBot();
+    await pluginLoader.runMount();
+    Bot.sendMessage = sendMessage;
+    Bot.makeForwardMsg = pluginLoader.makeForwardMsg;
+    Bot.renderImg = pluginLoader.renderImg;
+    const files = filemag.GetfileList().filter((file) => file.endsWith(".js"));
     for (let File of files) {
       try {
-        let listener = await import(`./Event/${File}`)
+        let listener = await import(`./Event/${File}`);
 
         /* eslint-disable new-cap */
-        if (!listener.default) continue
-        listener = new listener.default()
-        listener.client = this.client
-        const on = listener.once ? "once" : "on"
+        if (!listener.default) continue;
+        listener = new listener.default();
+        listener.client = this.client;
+        const on = listener.once ? "once" : "on";
 
         if (lodash.isArray(listener.event)) {
-          listener.event.forEach(type => {
-            const e = listener[type] ? type : "execute"
-            this.client[on](listener.prefix + type, event => {
-              this.bindEvent(event, botenv)
-              return listener[e](event)
-            })
-          })
+          listener.event.forEach((type) => {
+            const e = listener[type] ? type : "execute";
+            this.client[on](listener.prefix + type, (event) => {
+              this.bindEvent(event, botenv);
+              return listener[e](event);
+            });
+          });
         } else {
-          const e = listener[listener.event] ? listener.event : "execute"
-          this.client[on](listener.prefix + listener.event, event => {
-            this.bindEvent(event, botenv)
-            return listener[e](event)
-          })
+          const e = listener[listener.event] ? listener.event : "execute";
+          this.client[on](listener.prefix + listener.event, (event) => {
+            this.bindEvent(event, botenv);
+            return listener[e](event);
+          });
         }
       } catch (e) {
-        logger.mark(`监听事件错误：${File}`)
-        logger.error(e)
+        logger.mark(`监听事件错误：${File}`);
+        logger.error(e);
       }
     }
   }
 
   checkEnv() {
-    const Botkeys = Object.keys(this.client)
-    console.log(Object.keys(this.client))
+    const Botkeys = Object.keys(this.client);
+    console.log(Object.keys(this.client));
     if (Botkeys.includes("lain")) {
-      this.bot = this.client[this.client.botQQ]
-      return this.client[this.client.botQQ].adapter.name
+      this.bot = this.client[this.client.botQQ];
+      return this.client[this.client.botQQ].adapter.name;
     } else if (Botkeys.includes("uin") && Botkeys.includes("QQNT")) {
-      return "icqq"
+      return "icqq";
+    } else {
+      return this.client[this.client.botQQ]?.adapter.name;
     }
   }
 
   bindEvent(e, env) {
-    e.adapterType = "icqq"
-    const targetE = e
+    e.adapterType = "icqq";
+    const targetE = e;
     if (env === "OneBotv11") {
       const recallMessage = ({ peer_id, message_seq, isGroup }) => {
         try {
           if (isGroup) {
-            e.pickGroup(peer_id).recallMsg(e.message_id)
+            e.pickGroup(peer_id).recallMsg(e.message_id);
           } else {
-            e.pickFriend(peer_id).recallMsg(e.message_id)
+            e.pickFriend(peer_id).recallMsg(e.message_id);
           }
         } catch (error) {
-          console.log(error)
+          console.log(error);
         }
-      }
+      };
       e.sendGroupMessageReaction = ({ message_seq, reaction }) => {
-        this.bot.sendApi("set_msg_emoji_like", {
+        Bot[Bot.botQQ].sendApi("set_msg_emoji_like", {
           message_id: targetE.message_id,
           emoji_id: Number(reaction),
-        })
-      }
+        });
+      };
 
-      e.recallMessage = recallMessage
-      e.sendMessage = sendMessage
-      e.renderImg = pluginLoader.renderImg
+      e.recallMessage = recallMessage;
+      e.sendMessage = sendMessage;
+      e.renderImg = pluginLoader.renderImg;
+      e.getMsg = async (msg_id) => {
+        return await Bot[Bot.botQQ].sendApi("get_msg", {
+          message_id: msg_id,
+        });
+      };
     } else if (env === "icqq") {
       const recallMessage = ({ peer_id, message_seq, isGroup }) => {
         try {
           if (isGroup) {
-            Bot.pickGroup(peer_id).recallMsg(message_seq)
+            Bot.pickGroup(peer_id).recallMsg(message_seq);
           } else {
-            Bot.pickFriend(peer_id).recallMsg(message_seq)
+            Bot.pickFriend(peer_id).recallMsg(message_seq);
           }
         } catch (error) {
-          console.log(error)
+          console.log(error);
         }
-      }
+      };
       e.sendGroupMessageReaction = ({ message_seq, reaction }) => {
-        Bot.pickGroup(targetE.group_id).setReaction(targetE.seq, Number(reaction))
-      }
-      e.recallMessage = recallMessage
+        Bot.pickGroup(targetE.group_id).setReaction(
+          targetE.seq,
+          Number(reaction),
+        );
+      };
+      e.recallMessage = recallMessage;
 
-      e.getMsg = async msg_id => {
-        return await Bot.getMsg(msg_id)
-      }
-      e.getReplyMsg = async seq => {
+      e.getMsg = async (msg_id) => {
+        return await Bot.getMsg(msg_id);
+      };
+      e.getReplyMsg = async (seq) => {
         if (e.group_id) {
-          return await Bot.pickGroup(e.group_id).getChatHistory(seq, 1)
+          return await Bot.pickGroup(e.group_id).getChatHistory(seq, 1);
         }
-        return await Bot.pickFriend(e.user_id).getChatHistory(seq, 1)
-      }
-      e.getGroupMemberInfo = Bot.getGroupMemberInfo.bind(Bot)
+        return await Bot.pickFriend(e.user_id).getChatHistory(seq, 1);
+      };
+      e.getGroupMemberInfo = Bot.getGroupMemberInfo.bind(Bot);
     }
-    e.sendMessage = sendMessage
-    e.makeForwardMsg = pluginLoader.makeForwardMsg
-    e.renderImg = pluginLoader.renderImg
-    delete e.client
+    e.sendMessage = sendMessage;
+    e.makeForwardMsg = pluginLoader.makeForwardMsg;
+    e.renderImg = pluginLoader.renderImg;
+    delete e.client;
   }
 }
 
-export { ListenerLoader }
+export { ListenerLoader };
--- src/index.js
+++ src/index.js
@@ -1,64 +1,66 @@
 //如果是插件就通过全局BOT获取机器人实例，否则就选择作为api或者本体机器人
 function getQQFromArray(arr) {
   // QQ号规则：纯数字、长度4-13位、不以0开头
-  const qqReg = /^[1-9]\d{3,12}$/
+  const qqReg = /^[1-9]\d{3,12}$/;
   // 遍历数组，找到第一个匹配的QQ号
-  const qqItem = arr.find(item => qqReg.test(item))
-  return qqItem || "未找到有效QQ号"
+  const qqItem = arr.find((item) => qqReg.test(item));
+  return qqItem || "未找到有效QQ号";
 }
 async function getBotInstance() {
   try {
     if (Bot) {
       //插件环境下通过全局BOT获取机器人实例
-      console.log("云崽环境")
+      console.log("云崽环境");
       let timer = setInterval(async () => {
-        let qq = getQQFromArray(Object.keys(Bot))
+        let qq = getQQFromArray(Object.keys(Bot));
         if (qq != "未找到有效QQ号") {
-          clearInterval(timer)
+          clearInterval(timer);
           // console.log(bot);
-          const { ListenerLoader } = await import("./Bot/icqq/EventListener.js")
-          Bot.botQQ = qq
-          process.env.xunLuEnv = "QQBot-ICQQYunZai"
+          const { ListenerLoader } =
+            await import("./Bot/icqq/EventListener.js");
+          Bot.botQQ = qq;
+          process.env.xunLuEnv = "QQBot-ICQQYunZai";
 
-          new ListenerLoader().load(Bot)
+          new ListenerLoader().load(Bot);
         }
-      }, 10000)
+      }, 10000);
     }
   } catch (error) {
     //作为api或者本体机器人时，选择合适的机器人实例
-    await loadEnv()
-    loadLLbot().catch(async err => {
-      console.error("加载LLbot失败:", err)
-      console.log("开始加载api服务器")
+    await loadEnv();
+    loadLLbot().catch(async (err) => {
+      console.error("加载LLbot失败:", err);
+      console.log("开始加载api服务器");
       try {
-        await import("./Bot/onebotV11/event/index.js")
-        process.env.xunLuEnv = "QQBot-onebotV11"
-        console.log(process.env.xunLuEnv)
+        await import("./Bot/onebotV11/event/index.js");
+        process.env.xunLuEnv = "QQBot-onebotV11";
+        console.log(process.env.xunLuEnv);
       } catch (error) {
-        const { startServer } = await import("./lib/server.js")
-        startServer()
-        process.env.xunLuEnv = "API-Server"
-        console.log(process.env.xunLuEnv)
+        const { startServer } = await import("./lib/server.js");
+        startServer();
+        process.env.xunLuEnv = "API-Server";
+        console.log(process.env.xunLuEnv);
       }
-    })
-    console.log(process.env.xunLuEnv)
+    });
+    console.log(process.env.xunLuEnv);
   }
 }
 
 async function loadLLbot() {
-  const { default: EventListener } = await import("./Bot/llonebot/event/index.js")
-  await new EventListener().load()
-  process.env.xunLuEnv = "QQBot-LLoneBot"
-  console.log(process.env.xunLuEnv)
+  const { default: EventListener } =
+    await import("./Bot/llonebot/event/index.js");
+  await new EventListener().load();
+  process.env.xunLuEnv = "QQBot-LLoneBot";
+  console.log(process.env.xunLuEnv);
 }
 
 async function loadEnv() {
-  const { default: logjs } = await import("./component/logger/log.js")
-  await logjs()
-  const { default: startRedis } = await import("./component/redis/redis.js")
-  await startRedis()
+  const { default: logjs } = await import("./component/logger/log.js");
+  await logjs();
+  const { default: startRedis } = await import("./component/redis/redis.js");
+  await startRedis();
 }
 
-getBotInstance()
+getBotInstance();
 
-export default {}
+export default {};
